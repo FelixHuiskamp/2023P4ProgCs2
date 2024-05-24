71c638f7292e95cce8cc7750f455c9441e0591c0
FelixHuiskamp <37748@ma-web.nl>
2024-05-24 12:22:50 +0200

    1

---
+++ ListOpdracht/ListOpdracht/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ListOpdracht
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<string> birds = new List<string>() { "ui", "kraai", "hond", "papegaai"};
+          //  totalElements = birds.Count;
+            List<string> birds2 = new List<string>() { "meeuw", "duif" };
+            birds.AddRange(birds2);
+            birds.Remove("hond");
+            for (int i = 0; i < birds.Count; i++)
+            {
+                Console.WriteLine(birds[i]);
+
+            }
+
+
+        }
+    }
+}
