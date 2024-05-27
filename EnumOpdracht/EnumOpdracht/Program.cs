f60b041af216d7a2f2a5a224ec07364fbb5a77cb
FelixHuiskamp <37748@ma-web.nl>
2024-05-27 12:31:39 +0200

    1

---
+++ EnumOpdracht/EnumOpdracht/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Colors myMainThemeColor = Colors.Blue;
+
+            if (myMainThemeColor == Colors.Blue)
+            {
+
+                Console.WriteLine("dat is mijn kleur");
+
+            }
+            
+
+        }
+    }
+}
