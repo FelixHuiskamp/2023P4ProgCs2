a56afa8ade2aed039b7f770e7f9b56c88d080683
FelixHuiskamp <37748@ma-web.nl>
2024-06-03 11:59:09 +0200

    1

---
+++ FIleDirOpdracht/FIleDirOpdracht/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FIleDirOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\jedif\OneDrive\Documenten\ma\Periode 4 PROG C#\2023P4ProgCs2");
+
+
+           DirectoryInfo[] directoryInfos = dir.GetDirectories();
+            for (int i = 0; i < directoryInfos.Length; i++)
+            {
+                Console.WriteLine(directoryInfos[i].FullName);
+            }
+
+            
+        }
+    }
+}
