505b86453af883494fe824836ec68d02401b5edb
FelixHuiskamp <37748@ma-web.nl>
2024-06-10 12:51:37 +0200

    1

---
+++ FileIOOpdracht/FileIOOpdracht/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FileIOOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] content = File.ReadAllLines("leesdezefile.txt");
+            foreach (string line in content)
+            {
+                Console.WriteLine(line);
+            }
+
+
+            File.WriteAllText("mijnnieuwefile.txt","hello");
+            File.AppendAllText("mijnnieuwefile.txt", "hello2");
+            Directory.CreateDirectory("output"); 
+
+
+        }
+    }
+}
